ac8a90bb4b9b861ba0b32195643afeefd4683efd
Shubham <shubham.rohilla@sastechstudio.com>
2022-05-10 11:57:03 +0530

    initial commit

---
+++ src/main/java/com/sts/parallelCap/payment/ParallelCapApplication.java
@@ -0,0 +1,13 @@
+package com.sts.parallelCap.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelCapApplication.class, args);
+	}
+
+}
